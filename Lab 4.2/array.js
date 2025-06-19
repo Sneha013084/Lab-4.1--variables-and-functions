133c2710562fc43eb02509dd0e2a5ed8ac590d86
Sneha <snehayur30@gmail.com>
2025-06-19 15:12:43 -0400

    initial commit

---
+++ Lab 4.2/array.js
@@ -0,0 +1,35 @@
+let shoppingList = [];
+
+function addItem( item){
+     shoppingList.push(item);
+}
+function removeItem (){
+    shoppingList.pop();
+}
+function displayList(){
+    if(shoppingList.length===0){
+        console.log(" The shopping list is empty");
+    }else{
+        console.log("Shopping List:");
+
+        shoppingList.forEach ((item, index) =>{
+            console.log(`${index+1}.${item}`)
+
+        });
+    }
+}
+
+// testing
+   
+  addItem("Milk");
+  addItem("egg");
+  addItem ("Bread");
+
+  displayList();
+
+  removeItem();
+
+  displayList ();
+
+  removeItem();
+  displayList();
