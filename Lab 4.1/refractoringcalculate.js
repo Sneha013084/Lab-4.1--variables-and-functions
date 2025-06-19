7aa754e203ab8b75a279de6a9439f52743b31d76
Sneha <snehayur30@gmail.com>
2025-06-19 12:10:59 -0400

    initial commit

---
+++ Lab 4.1/refractoringcalculate.js
@@ -0,0 +1,12 @@
+function calculateTotalCost (price, quantity, taxRate, discount = 0){
+    if (typeof price != "number" || typeof quantity != " number" || typeof taxRate != "number" ||typeof discount != "number") {
+
+        return "Invalid output";
+    }
+    const subtotal = price *quantity;
+
+    const adjustedSubtotal =  subtotal< 0 ? 0 : subtotal;
+
+    const totalCost = adjustedSubtotal *(1+taxRate);
+    return taxRate.toFixed(2);
+}
